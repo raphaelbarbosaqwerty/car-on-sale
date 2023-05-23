c0d9422551cad2864cec0a1f8b3397cf4fae0b7e
raphaelbarbosaqwerty <raphaelcorreabarbosa@hotmail.com>
2023-05-23 18:07:15 -0300

    Moved Singleton to LazySingleton

--- challenge/lib/app/core/di.dart
+++ challenge/lib/app/core/di.dart
@@ -1,16 +1,22 @@
 import 'package:challenge/app/core/domain/services/auth_service.dart';
 import 'package:challenge/app/core/domain/usecases/validate_cached_user.dart';
 import 'package:challenge/app/core/infra/services/auth_service.dart';
+import 'package:challenge/app/modules/auth/domain/save_user_data.dart';
 import 'package:challenge/app/modules/splash/presenter/splash_cubit.dart';
 import 'package:get_it/get_it.dart';
 
 final locator = GetIt.I;
 void setupLocator() {
-  locator.registerSingleton<IAuthService>(AuthService());
-  locator.registerSingleton<IValidateCachedUser>(ValidateCachedUser(
-    locator.get(),
-  ));
-  locator.registerSingleton<SplashCubit>(SplashCubit(
-    locator.get(),
-  ));
+  locator.registerLazySingleton<ISaveUserData>(() => SaveUserData());
+  locator.registerLazySingleton<IAuthService>(() => AuthService());
+  locator.registerLazySingleton<IValidateCachedUser>(
+    () => ValidateCachedUser(
+      locator.get(),
+    ),
+  );
+  locator.registerLazySingleton<SplashCubit>(
+    () => SplashCubit(
+      locator.get(),
+    ),
+  );
 }
